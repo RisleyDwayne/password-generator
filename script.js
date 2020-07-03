3ce7189a0a747e244e6a329d01a366c006c00245
Dwayne Risley <risleydwayne@gmail.com>
2020-07-03 00:42:01 -0400

    started js

---
+++ script.js
@@ -0,0 +1,8 @@
+const generateButton = document.querySelector('#generateButton');
+
+const length = document.querySelector('#length');
+const lower = document.querySelector('#lowercase');
+const upper = document.querySelector('#uppercase');
+const numbers = document.querySelector('#numbers');
+const specialChar = document.querySelector('#special');
+
